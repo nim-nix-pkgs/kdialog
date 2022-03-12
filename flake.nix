a204f0fb8d5e79ca1bd5cb88017798e4a7a15182
hugosenari <hugosenari@gmail.com>
2022-03-12 01:32:13 -0300

    chore: re index kdialog

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''KDialog Qt5 Wrapper, easy API, KISS design'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."kdialog-master".url = "path:./master";
+  inputs."kdialog-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."kdialog-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
